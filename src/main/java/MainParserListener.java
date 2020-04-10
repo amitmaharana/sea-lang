dc6428976ed4a6fd01f1239bac189fe5810f959e
Shwetank-bhardwaj <shwetank.grad@gmail.com>
2020-04-09 17:38:47 -0700

    comments added to better explain grammar listener changes

--- src/main/java/MainParserListener.java
+++ src/main/java/MainParserListener.java
@@ -5,43 +5,6 @@ public class MainParserListener extends SEALangBaseListener {
 	HashMap<String, Object> variableMap = new HashMap<>();
 
 	@Override
-	public void exitLet(SEALangParser.LetContext ctx) {
-		/*if (ctx.TYPE() == null) {
-			return;
-		}
-		switch (ctx.TYPE().getText()) {
-		case "Int":
-			if (ctx.INT() == null) {
-				this.variableMap.put(ctx.VAR().getText(), 0);
-			} else {
-				this.variableMap.put(ctx.VAR().getText(), Integer.parseInt(ctx.INT().getText()));
-			}
-			break;
-		case "Boolean":
-			this.variableMap.put(ctx.VAR().getText(), Boolean.parseBoolean(ctx.BOOLEAN().getText()));
-			break;
-		case "String":
-			this.variableMap.put(ctx.VAR().getText(), ctx.STRING().getText());
-			break;
-		case "Float":
-			this.variableMap.put(ctx.VAR().getText(), ctx.VAR().getText());
-			break;
-		}*/
-		switch (ctx.TYPE().getText()){
-			case "Int":
-
-				break;
-			case "Boolean":
-				break;
-			case "String":
-				break;
-			default:
-				System.out.println("Type not recognised");
-				break;
-		}
-	}
-
-	@Override
 	public void exitCommand(SEALangParser.CommandContext ctx) {
 		/*
 		 * if(ctx.VAR().size() == 2){ this.variableMap.put(ctx.VAR(0).getText(),
